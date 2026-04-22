857354d9d9a615d462d228b3c62e7dfdd4d4cd31
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-04-22 23:52:05 +0600

    Sacred Entry Store Developed

---
+++ lib/features/sacred_entry/presentation/widget/sacred_entry_store.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/foundation.dart';
+
+class SacredEntryItem {
+  const SacredEntryItem({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.wordCount,
+    required this.entryDate,
+    required this.typeLabel,
+    required this.typeIconAsset,
+  });
+
+  final String id;
+  final String title;
+  final String content;
+  final int wordCount;
+  final DateTime entryDate;
+  final String typeLabel;
+  final String typeIconAsset;
+}
+
+class SacredEntryStore {
+  SacredEntryStore._();
+
+  static final SacredEntryStore instance = SacredEntryStore._();
+
+  final ValueNotifier<List<SacredEntryItem>> _itemsNotifier =
+      ValueNotifier<List<SacredEntryItem>>(<SacredEntryItem>[]);
+
+  ValueListenable<List<SacredEntryItem>> get itemsListenable => _itemsNotifier;
+
+  void addItem({
+    required String title,
+    required String content,
+    required int wordCount,
+    required DateTime entryDate,
+    required String typeLabel,
+    required String typeIconAsset,
+  }) {
+    final SacredEntryItem newItem = SacredEntryItem(
+      id: DateTime.now().microsecondsSinceEpoch.toString(),
+      title: title,
+      content: content,
+      wordCount: wordCount,
+      entryDate: entryDate,
+      typeLabel: typeLabel,
+      typeIconAsset: typeIconAsset,
+    );
+
+    final List<SacredEntryItem> currentItems =
+        List<SacredEntryItem>.from(_itemsNotifier.value);
+    _itemsNotifier.value = <SacredEntryItem>[newItem, ...currentItems];
+  }
+}
