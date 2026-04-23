f8ba3de30ae9326cd26cb1239c4979c26409233f
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-04-24 04:03:40 +0600

    Wavy Separator Widget refactored to common widgets

---
+++ lib/common_widgets/wavy_separator.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../gen/colors.gen.dart';
+
+class WavySeparator extends StatelessWidget {
+  const WavySeparator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 5.h,
+      child: CustomPaint(
+        painter: WavyLinePainter(),
+      ),
+    );
+  }
+}
+
+class WavyLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = AppColors.cF5EDD7.withValues(alpha: 0.5)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.5.w;
+
+    final Path path = Path();
+    const double waves = 15;
+    final double segment = size.width / waves;
+
+    path.moveTo(0, size.height / 2);
+    for (double i = 0; i < waves; i++) {
+      final double startX = i * segment;
+      final double endX = startX + segment;
+      path.quadraticBezierTo(
+        startX + segment / 2,
+        i % 2 == 0 ? 0 : size.height,
+        endX,
+        size.height / 2,
+      );
+    }
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
