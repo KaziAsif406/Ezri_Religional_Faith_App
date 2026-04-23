3755cc725b784b6b659b5318b668d9ac86db94d0
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-04-24 04:46:11 +0600

    debug

--- lib/features/journey/presentation/journey.dart
+++ lib/features/journey/presentation/journey.dart
@@ -3,19 +3,13 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:template_flutter/common_widgets/dual_tone_title.dart';
 import 'package:template_flutter/constants/text_font_style.dart';
-import 'package:template_flutter/features/community/presentation/widget/community_swipe_card_deck.dart';
-import 'package:template_flutter/features/faith_anchors/presentation/widget/faith_anchor_swipe_card_deck.dart';
-import 'package:template_flutter/features/home/presentation/widget/sacred_entries_list.dart';
 import 'package:template_flutter/features/journey/presentation/widget/journey_reflection.dart';
-import 'package:template_flutter/features/sacred_entry/presentation/widget/sacred_entry_store.dart';
 import 'package:template_flutter/gen/assets.gen.dart';
 import 'package:template_flutter/helpers/all_routes.dart';
 import 'package:template_flutter/helpers/navigation_service.dart';
 import 'package:template_flutter/helpers/ui_helpers.dart';
 
 import '../../../gen/colors.gen.dart';
-import '../../../common_widgets/custom_section_card.dart';
-import '../../../common_widgets/custom_section_header.dart';
 
 
 class JourneyScreen extends StatefulWidget {
