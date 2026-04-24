63af9f108faa9dd2e9b875c5d9d1f7d956dd24e8
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-04-25 03:33:16 +0600

    Add New Goal Screen Developed

---
+++ lib/features/goals/presentation/add_goal.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:template_flutter/common_widgets/custom_button.dart';
+import 'package:template_flutter/common_widgets/wavy_separator.dart';
+import 'package:template_flutter/constants/text_font_style.dart';
+import 'package:template_flutter/features/goals/presentation/widget/frequency_selector.dart';
+import 'package:template_flutter/features/goals/presentation/widget/goal_type_selector.dart';
+import 'package:template_flutter/features/goals/presentation/widget/reminder.dart';
+import 'package:template_flutter/gen/assets.gen.dart';
+import 'package:template_flutter/gen/colors.gen.dart';
+import 'package:template_flutter/helpers/navigation_service.dart';
+
+class AddGoalScreen extends StatefulWidget {
+  const AddGoalScreen({super.key});
+
+  @override
+  State<AddGoalScreen> createState() => _AddGoalScreenState();
+}
+
+class _AddGoalScreenState extends State<AddGoalScreen> {
+  GoalType _selectedGoalType = GoalType.prayer;
+  GoalFrequency _selectedFrequency = GoalFrequency.weekly;
+  final Set<int> _selectedDays = <int>{2};
+  bool _isReminderOn = true;
+  TimeOfDay _selectedTime = const TimeOfDay(hour: 9, minute: 0);
+
+  bool get _showDaySelector {
+    return _selectedFrequency == GoalFrequency.weekly ||
+        _selectedFrequency == GoalFrequency.monthly;
+  }
+
+  Future<void> _pickReminderTime() async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: const ColorScheme.light(
+              primary: AppColors.allsecondaryColor,
+            ),
+          ),
+          child: child ?? const SizedBox.shrink(),
+        );
+      },
+    );
+
+    if (newTime != null) {
+      setState(() {
+        _selectedTime = newTime;
+      });
+    }
+  }
+
+  void _toggleDay(int dayIndex) {
+    setState(() {
+      if (_selectedDays.contains(dayIndex)) {
+        _selectedDays.remove(dayIndex);
+      } else {
+        _selectedDays.add(dayIndex);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldColor,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 300.h,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(Assets.images.header1.path),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    height: 100.h,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          AppColors.scaffoldColor.withValues(alpha: 0),
+                          AppColors.scaffoldColor,
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(24.w, 18.h, 24.w, 0),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () => NavigationService.goBack,
+                          child: Container(
+                            width: 64.w,
+                            height: 64.w,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color:
+                                    AppColors.cF5EDD7.withValues(alpha: 0.75),
+                                width: 1.6.w,
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.arrow_back_ios_new_rounded,
+                              color: AppColors.cF5EDD7,
+                              size: 22.sp,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 16.w),
+                        Text(
+                          'Add New Goal',
+                          style:
+                              TextFontStyle.textStyle32cFFFFFFHelveticaNeue700,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(24.w, 12.h, 24.w, 30.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: TextFontStyle.textStyle24c8C7C68HelveticaNeue400,
+                      children: [
+                        TextSpan(
+                          text: 'What\'s the ',
+                          style:
+                              TextFontStyle.textStyle24c8C7C68HelveticaNeue400,
+                        ),
+                        TextSpan(
+                          text: 'goal type?',
+                          style:
+                              TextFontStyle.textStyle24c3B230EHelveticaNeue500,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+                  GoalTypeSelector(
+                    selectedType: _selectedGoalType,
+                    onTypeChanged: (GoalType newType) {
+                      setState(() {
+                        _selectedGoalType = newType;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 28.h),
+                  RichText(
+                    text: TextSpan(
+                      style: TextFontStyle.textStyle24c8C7C68HelveticaNeue400,
+                      children: [
+                        TextSpan(
+                          text: 'How often do you ',
+                          style:
+                              TextFontStyle.textStyle24c8C7C68HelveticaNeue400,
+                        ),
+                        TextSpan(
+                          text: 'want to do this?',
+                          style:
+                              TextFontStyle.textStyle24c3B230EHelveticaNeue500,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 14.h),
+                  FrequencySelector(
+                    selectedFrequency: _selectedFrequency,
+                    onFrequencyChanged: (GoalFrequency newFrequency) {
+                      setState(() {
+                        _selectedFrequency = newFrequency;
+                      });
+                    },
+                  ),
+                  if (_showDaySelector) ...[
+                    SizedBox(height: 24.h),
+                    WavySeparator(
+                        color: AppColors.cF5EDD7.withValues(alpha: 0.45)),
+                    SizedBox(height: 24.h),
+                    RichText(
+                      text: TextSpan(
+                        style: TextFontStyle.textStyle24c8C7C68HelveticaNeue400,
+                        children: [
+                          TextSpan(
+                            text: 'Select ',
+                            style: TextFontStyle
+                                .textStyle24c8C7C68HelveticaNeue400,
+                          ),
+                          TextSpan(
+                            text: 'the day(s)',
+                            style: TextFontStyle
+                                .textStyle24c3B230EHelveticaNeue500,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 14.h),
+                    DaySelector(
+                      selectedDays: _selectedDays,
+                      onDayToggle: _toggleDay,
+                    ),
+                  ],
+                  SizedBox(height: 24.h),
+                  WavySeparator(
+                      color: AppColors.cF5EDD7.withValues(alpha: 0.45)),
+                  SizedBox(height: 24.h),
+                  ReminderSection(
+                    isReminderEnabled: _isReminderOn,
+                    onReminderToggle: (bool value) {
+                      setState(() {
+                        _isReminderOn = value;
+                      });
+                    },
+                    selectedTime: _selectedTime,
+                    onTimeTap: _pickReminderTime,
+                  ),
+                  SizedBox(height: 32.h),
+                  CustomButton(
+                    onPressed: () {},
+                    title: 'Save Goal',
+                    height: 72.h,
+                    borderRadius: BorderRadius.circular(999.r),
+                    backgroundGradient: const LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        AppColors.cC7BFAD,
+                        AppColors.c6B2F45,
+                      ],
+                    ),
+                    textStyle: TextFontStyle.textStyle32cFFFFFFHelveticaNeue500
+                        .copyWith(
+                      fontSize: 36.sp,
+                      fontWeight: FontWeight.w700,
+                      color: AppColors.cF5EDD7,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
