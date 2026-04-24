34cd9d1f0fb572608766b3e345f5ac22bcb10d0f
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-04-25 00:09:21 +0600

    Milestone screen header Developed

---
+++ lib/features/milestone/presentation/widget/milestone_header.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:template_flutter/common_widgets/circular_progress_button.dart';
+import 'package:template_flutter/common_widgets/custom_appbar.dart';
+import 'package:template_flutter/constants/text_font_style.dart';
+import 'package:template_flutter/features/milestone/presentation/widget/milestone_summary.dart';
+import 'package:template_flutter/gen/assets.gen.dart';
+import 'package:template_flutter/gen/colors.gen.dart';
+import 'package:template_flutter/helpers/navigation_service.dart';
+
+class MilestoneHeader extends StatelessWidget {
+  const MilestoneHeader({
+    super.key,
+    this.completedDays = 3,
+    this.targetDays = 7,
+    this.currentStreak = 5,
+    this.userName = 'Wade',
+    this.onContinueTap,
+  });
+
+  final int completedDays;
+  final int targetDays;
+  final int currentStreak;
+  final String userName;
+  final VoidCallback? onContinueTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final double progress = targetDays == 0 ? 0 : completedDays / targetDays;
+    final int remainingDays = (targetDays - completedDays).clamp(0, targetDays);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomAppBar(
+          titleText: 'Milestone Progress',
+          imageHeight: 380.0.h,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Transform.translate(
+            offset: Offset(0, -230.h),
+            child: Column(
+              children: [
+                WeeklyProgressIndicator(
+                  completedDays: completedDays,
+                  selectedBackgroundColor: AppColors.cF5F6F5.withValues(alpha: 0.12),
+                  unselectedBackgroundColor: AppColors.allsecondaryColor.withValues(alpha: 0.28),
+                ),
+                SizedBox(height: 70.h),
+                MilestoneSummaryCard(
+                  completedDays: completedDays,
+                  targetDays: targetDays,
+                  currentStreak: currentStreak,
+                  userName: userName,
+                  remainingDays: remainingDays,
+                  progress: progress,
+                  onContinueTap: onContinueTap,
+                ),
+              ],
+            ),
+          ),
+        ),
+        
+      ],
+    );
+  }
+}
+
+
